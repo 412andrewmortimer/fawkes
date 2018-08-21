6e53ed14cc40bf83c0f518fc76ab1c67792ca1dc
Andrew Mortimer <andrcmdr@protonmail.com>
2018-08-21 11:42:16 -0400

    Config for running in devlopment mode locally

--- config/dev.exs
+++ config/dev.exs
@@ -52,7 +52,7 @@ config :phoenix, :stacktrace_depth, 20
 config :fawkes, Fawkes.Repo,
   adapter: Ecto.Adapters.Postgres,
   username: "postgres",
-  password: "",
+  password: "postgres",
   database: "fawkes_dev",
-  hostname: "db",
+  hostname: "localhost",
   pool_size: 10
